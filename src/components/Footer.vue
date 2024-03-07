1ddf8dec82886a0c214c32f4b20b5b612d30cf0b
nikola7Kmotobe <nikolamotobe@gmail.com>
2024-03-06 23:21:43 -0300

    Arrumando rotas

---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<script setup>
+
+</script>
+<template>
+    <div><h1>foootteerrr</h1></div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/pedidos">Pedidos</RouterLink>
+          <RouterView />
+    </nav>
+</template>
+
