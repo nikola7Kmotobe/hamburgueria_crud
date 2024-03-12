5e37c1379f26706a401768028e845353f197f6d3
nikola7Kmotobe <nikolamotobe@gmail.com>
2024-03-11 23:15:08 -0300

    Adicionando o Form e resgatando dados do BD

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,12 @@
   <Banner />
     <div class="main-container">
       <h1>Monte o seu burger:</h1>
+      <Form />
     </div>
   </main>
 </template>
 
 <script setup>
  import Banner from '../components/Banner.vue'
+ import Form from '../components/Form.vue'
 </script>
